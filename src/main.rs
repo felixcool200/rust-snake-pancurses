4ef22a73506b4aa4f461e9fa41a7c49b79c11f3e
felixcool200 <felixsoderman@gmail.com>
2023-05-16 23:01:43 +0200

    Added a first version of a welcome screen

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 //extern crate pancurses;
+//use std::collections::HashMap;
+
 pub mod food;
 pub mod game;
 pub mod point;
@@ -6,7 +8,39 @@ pub mod snake;
 
 pub use game::Game;
 
-use pancurses::{curs_set, endwin, initscr, noecho, Input};
+use pancurses::{curs_set, endwin, initscr, napms, noecho, Input, Window};
+
+struct Key<'a> {
+    printable: &'a str,
+    input: Input,
+}
+
+const PAUSE_KEY: Key = Key {
+    printable: "Backspace",
+    input: Input::KeyBackspace,
+};
+
+const QUIT_KEY: Key = Key {
+    printable: "Delete",
+    input: Input::KeyDC,
+};
+
+fn print_welcome_text(window: &Window, game: &Game) {
+    let lines: [String; 3] = [
+        "Welcome to Snake (written in Rust)".to_owned(),
+        format!("Press {0} to pause!", PAUSE_KEY.printable).to_owned(),
+        format!("Press {0} to quit!", QUIT_KEY.printable),
+    ];
+    for (index, line) in lines.iter().enumerate() {
+        window.mvprintw(
+            game.height / 2 + index as i32,
+            (game.width - line.len() as i32) / 2,
+            line,
+        );
+    }
+    window.refresh();
+    napms(2000);
+}
 
 fn main() {
     let window = initscr();
@@ -17,6 +51,9 @@ fn main() {
     window.timeout(1); // Do not wait for input
     noecho();
     curs_set(0);
+
+    print_welcome_text(&window, &game);
+
     loop {
         match window.getch() {
             Some(Input::Character(c)) => {
